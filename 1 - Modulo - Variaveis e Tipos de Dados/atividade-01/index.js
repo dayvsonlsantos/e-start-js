21b394485d6958af21140f188ebfaf0de671c28d
Dayvson Lima <dayvsonlsantos@gmail.com>
2022-08-25 20:01:28 -0300

    Feat: Atividade 01 - Criar variáveis

---
+++ 1 - Modulo - Variaveis e Tipos de Dados/atividade-01/index.js
@@ -0,0 +1,16 @@
+//1 - Criar variáveis com tipos de dados:
+
+//a) Number
+    let numero = 2;
+//b) String
+    let nome = "Pedro"
+//c) Boolean
+    let luzLigada = true;
+//d) Array
+    let itens = [];
+//e) Object
+    let objetos = {};
+//f) Undefined
+    let viagem;
+//g) Null
+    let racaGatos = null;
