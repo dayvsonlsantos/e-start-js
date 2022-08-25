06660d9dce5cc991e97b4682866c704ccb36eb68
Dayvson Lima <dayvsonlsantos@gmail.com>
2022-08-25 20:07:40 -0300

    Feat: Atividade 02 - Variáveis

---
+++ 1 - Modulo - Variaveis e Tipos de Dados/atividade-02/index.js
@@ -0,0 +1,14 @@
+//1) Criar duas variáveis usando a palavra-chave var.
+
+    var nome = "João";
+    var sobrenome = "Ferreira";
+
+//2) Criar duas variáveis usando a palavra-chave let.
+
+    let endereco = "Rua Ana, 248";
+    let cidade = "Recife";
+
+//3) Criar duas constantes usando a palavra-chave const.
+
+    const CPF = "24935655410";
+    const datanasc = "28/10/2001";
