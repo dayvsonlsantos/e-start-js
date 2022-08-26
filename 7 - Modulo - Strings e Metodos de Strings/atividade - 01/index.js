c750e916c6a51cb84e664bc4964bd88330c73a56
Dayvson Lima <dayvsonlsantos@gmail.com>
2022-08-26 00:24:15 -0300

    Feat: Atividade - 01

---
+++ 7 - Modulo - Strings e Metodos de Strings/atividade - 01/index.js
@@ -0,0 +1,46 @@
+//Criar um arquivo javascript com as atividade 1 e 2
+
+//1)      Criar 3 variáveis:
+
+///a)       A primeira variável deverá conter o seu nome. 
+
+let nome = "Dayvson"
+
+//b)      A segunda variável deverá conter a sua idade.
+
+let idade = 20
+
+//c)       Converter a segunda variável para string.
+
+idade = idade.toString();
+
+//d)      A terceira variável deverá conter a concatenação das duas variáveis anteriores.
+
+let concatenacao = nome + " " + idade
+
+//e)      Exibir no console a terceira variável.
+
+console.log(concatenacao)
+
+//f)        Exibir no console o tamanho da terceira variável.
+
+console.log(concatenacao.length)
+
+
+//2)      Criar 2 variáveis:
+
+//a)       A primeira variável deverá conter o valor 17.
+
+let valor1 = 17;
+
+//b)      A segunda variável deverá conter o valor 28.
+
+let valor2 = 28;
+
+//c)       Converter os valores das duas variáveis
+
+let valor3 = valor1.toString() + valor2.toString();
+
+//d)       Exibir no console a terceira variável.
+
+console.log(valor3)
