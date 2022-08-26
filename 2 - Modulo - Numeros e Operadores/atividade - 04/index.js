84f24e5efb03e18158b1c120e4425a79cac384a2
Dayvson Lima <dayvsonlsantos@gmail.com>
2022-08-25 21:25:13 -0300

    Feat: Atividade - 04

---
+++ 2 - Modulo - Numeros e Operadores/atividade - 04/index.js
@@ -0,0 +1,91 @@
+//1)      Criar variáveis para:
+
+//a)       Armazenar os valores: 7, -55, 389, 1000, 3.1459, -46.267;
+
+//b)      As variáveis deverão ser do tipo objeto Number;
+
+let num1 = new Number(7);
+
+let num2 = new Number(-55);
+
+let num3 = new Number(389);
+
+let num4 = new Number(1000);
+
+let num5 = new Number(3.1459);
+
+let num6 = new Number(-46.267);
+
+//c)       Mostrar os valores no console;
+
+console.log(num1 + " " + num2 + " " + num3 + " " + num4 + " " + num5 + " " + num6)
+
+//d)      Somar a primeira variável com a última variável;
+
+let soma = num1 + num6;
+
+//e)      Mostrar os valores no console;
+
+console.log(soma)
+
+//f)        Multiplicar a primeira variável com a terceira variável;
+
+let multiplicar = num1 * num3;
+
+//g)       Mostrar os valores no console;
+
+console.log(multiplicar)
+
+//h)      Dividir a quarta variável pela quinta variável;
+
+let dividir = num4 / num5;
+
+//i)        Mostrar os valores no console.
+
+console.log(dividir)
+
+
+
+//2)      Criar variáveis para:
+
+//a)       Armazenar os valores: 10, 3, 9, 99.9, 721;
+
+//b)      As variáveis deverão ser do tipo objeto Number.
+
+let valor1 = new Number(10);
+
+let valor2 = new Number(3);
+
+let valor3 = new Number(9);
+
+let valor4 = new Number(99.9);
+
+let valor5 = new Number(721);
+
+//c)       Mostrar os valores no console.
+
+console.log (valor1 + " " + valor2 + " " + valor3 + " " + valor4 + " " + valor5);
+
+//d)      Validar se a primeira variável é menor que o valor Number.MAX_VALUE;
+
+console.log(valor1 < Number.MAX_VALUE)
+
+//e)      Mostrar o resultado e valores no console;
+
+console.log (valor1 + " " + valor2 + " " + valor3 + " " + valor4 + " " + valor5);
+
+//f)        Validar se a segunda variável possui valor igual a NaN;
+
+console.log(valor2 < Number.NaN);
+
+//g)       Mostrar o resultado e valores no console;
+
+console.log (valor1 + " " + valor2 + " " + valor3 + " " + valor4 + " " + valor5);
+
+//h)      Validar se a quinta variável é menor ou igual a terceira variável;
+
+console.log(valor5 <= valor3);
+
+//i)        Mostrar o resultado e valores no console.
+
+console.log (valor1 + " " + valor2 + " " + valor3 + " " + valor4 + " " + valor5);
