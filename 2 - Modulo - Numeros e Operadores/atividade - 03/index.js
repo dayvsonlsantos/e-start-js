962f7e2a19f62eb3b900bb28e3647b689d2f5744
Dayvson Lima <dayvsonlsantos@gmail.com>
2022-08-25 21:11:06 -0300

    Feat: Atividade 03

---
+++ 2 - Modulo - Numeros e Operadores/atividade - 03/index.js
@@ -0,0 +1,46 @@
+//1)      Criar bloco de código que mostra, no console, o resto da divisão de 25 por 3.
+
+let valor1 = 25;
+let valor2 = 3;
+
+console.log(valor1 % valor2);
+
+//2)      Criar bloco de código que mostra, no console, a concatenação das variáveis abaixo:
+
+//a)       texto1 = "Programa Start - "
+
+    let texto1 = "Programa Start - ";
+
+//b)      texto2 = "formação Front end: "
+
+    let texto2 = "formação Front end: ";
+
+//c)       texto3 = "eu faço parte dessa oportunidade."
+
+    let texto3 = "eu faço parte dessa oportunidade";
+
+    console.log(texto1 + texto2 + texto3)
+
+//3)      Criar bloco de código que mostra, no console, o resultado da concatenação das variáveis abaixo:
+
+//a)       texto1 = "Mariazinha tem "
+
+let text1 = "Mariazinha tem ";
+
+//b)      idade =  9
+
+let idade = 9;
+
+//c)       texto2 = "anos e "
+
+let text2 = "anos e ";
+
+//d)      quantidade_irmaos = 7
+
+let quantidade_irmaos = 7;
+
+//e)      texto3 = " irmãos."
+
+let text3 = " irmãos."
+
+console.log(text1 + idade + " " + text2 + quantidade_irmaos + text3);
