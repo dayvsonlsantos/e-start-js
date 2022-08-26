63ddb73a47d3232f9e742ecc2f19c159f7a557c9
Dayvson Lima <dayvsonlsantos@gmail.com>
2022-08-25 21:43:20 -0300

    Feat: Atividade - 02

---
+++ 3 - Modulo - Booleanos e Condicionais/atividade - 02/index.js
@@ -0,0 +1,31 @@
+//1)      Exibir no console o resultado das seguintes validações, dado o valor da variável sistema_operacional é linux:
+
+let sistema_operacional = prompt("Informe qual é o seu sistema operacional? \n linux, windows, macOS, Chrome OS")
+
+//a)       Se o valor for linux, mostrar: O Linux é um sistema operacional de código aberto.
+
+//b)      Se o valor for windows, mostrar: O Windows é um sistema operacional da empresa Microsoft.
+
+//c)       Se o valor for macOS, mostrar: O macOS é um sistema operacional da empresa Apple.
+
+//d)      Se o valor for Chrome OS, mostrar: O Chrome OS é um sistema operacional da empresa Google.
+
+//e)      Se não for nenhuma das opções acima, mostrar: Há a possibilidade de você estar utilizando um sistema mobile.
+
+switch (sistema_operacional) {
+    case "linux":
+        console.log("O Linux é um sistema operacional de código aberto")
+        break;
+    case "windows":
+        console.log("O Windows é um sistema operacional da empresa Microsoft")
+        break;
+    case "macOS":
+        console.log("O macOS é um sistema operacional da empresa Apple")
+        break;
+    case "Chrome OS":
+        console.log("O Chrome OS é um sistema operacional da empresa Google")
+        break;
+    default:
+        console.log("Há a possibilidade de você estar utilizando um sistema mobile");
+        break;
+}
