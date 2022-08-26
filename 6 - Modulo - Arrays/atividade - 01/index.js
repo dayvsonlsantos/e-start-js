d0764e2c0e0498a73b1a8906afcf32cfa6f40dbf
Dayvson Lima <dayvsonlsantos@gmail.com>
2022-08-25 23:15:30 -0300

    Feat: Atividade - 01

---
+++ 6 - Modulo - Arrays/atividade - 01/index.js
@@ -0,0 +1,52 @@
+//1)      Criar Array com 10 itens ( de 1 a 10).
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//a)       Exibir no console o total de itens.
+
+console.log(numeros.length-1);
+
+//b)      Exibir no console os itens nas posições: 1, 3, 5, 8.
+
+console.log(numeros[1])
+console.log(numeros[3])
+console.log(numeros[5])
+console.log(numeros[8])
+
+//2)      Criar Array com 5 itens (nome de filmes).
+
+let filmes = ["De volta para o Futuro", "Matrix", "Homem Aranha", "Vingadores", "Minions"]
+
+//a)       Exibir no console o total de itens.
+
+console.log(filmes.length-1);
+
+//b)      Exibir no console os itens nas posições: 2, 4.
+
+console.log(filmes[2])
+console.log(filmes[4])
+
+//c)       Exibir no console o item na primeira posição.
+
+console.log(filmes[0])
+
+
+
+//3)      Criar Array com 6 itens (nome de músicas).
+
+let musicas = ["Era Deus", "Início e Fim", "Lapidação", "Me Acolheu", "Liberta-Me de Mim", "All I Need Is You"]
+
+//a)       Exibir no console o total de itens.
+
+console.log(musicas.length-1);
+
+//b)      Exibir no console os itens nas posições : 1, 2, 6, 8.
+
+console.log(musicas[1]);
+console.log(musicas[2]);
+console.log(musicas[6]);
+console.log(musicas[8]);
+
+//c)       Exibir no console o item na última posição.
+
+console.log(musicas[musicas.length-1]);
